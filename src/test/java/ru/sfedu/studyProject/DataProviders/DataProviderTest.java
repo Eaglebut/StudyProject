4b1dca44713cab14f6e15a7f913207cf0d484d0f
Eaglebutt <Eaglebuttt@gmail.com>
2020-11-10 16:12:39 +0300

    Created test file to project

---
+++ src/test/java/ru/sfedu/studyProject/DataProviders/DataProviderTest.java
@@ -0,0 +1,167 @@
+package ru.sfedu.studyProject.DataProviders;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataProviderTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void createTask() {
+    }
+
+    @Test
+    public void testCreateTask() {
+    }
+
+    @Test
+    public void getOwnTasks() {
+    }
+
+    @Test
+    public void deleteTask() {
+    }
+
+    @Test
+    public void editTask() {
+    }
+
+    @Test
+    public void getProfileInformation() {
+    }
+
+    @Test
+    public void testGetProfileInformation() {
+    }
+
+    @Test
+    public void changeProfileInformation() {
+    }
+
+    @Test
+    public void joinTheGroup() {
+    }
+
+    @Test
+    public void createGroup() {
+    }
+
+    @Test
+    public void setGroupPrivate() {
+    }
+
+    @Test
+    public void setGroupPasswordedWithConfirmation() {
+    }
+
+    @Test
+    public void searchGroupByName() {
+    }
+
+    @Test
+    public void searchGroupById() {
+    }
+
+    @Test
+    public void getFullGroupList() {
+    }
+
+    @Test
+    public void getGroupTasks() {
+    }
+
+    @Test
+    public void getGroupProfile() {
+    }
+
+    @Test
+    public void getGroupAndOwnTasks() {
+    }
+
+    @Test
+    public void leaveGroup() {
+    }
+
+    @Test
+    public void testGetGroupTasks() {
+    }
+
+    @Test
+    public void testGetGroupProfile() {
+    }
+
+    @Test
+    public void getGroupMemberList() {
+    }
+
+    @Test
+    public void offerTaskToGroup() {
+    }
+
+    @Test
+    public void testOfferTaskToGroup() {
+    }
+
+    @Test
+    public void testOfferTaskToGroup1() {
+    }
+
+    @Test
+    public void changeGroupProfile() {
+    }
+
+    @Test
+    public void createGroupTask() {
+    }
+
+    @Test
+    public void testCreateGroupTask() {
+    }
+
+    @Test
+    public void editGroupTask() {
+    }
+
+    @Test
+    public void deleteGroupTask() {
+    }
+
+    @Test
+    public void banUser() {
+    }
+
+    @Test
+    public void unbanUser() {
+    }
+
+    @Test
+    public void acceptTask() {
+    }
+
+    @Test
+    public void declineTask() {
+    }
+
+    @Test
+    public void acceptUser() {
+    }
+
+    @Test
+    public void declineUser() {
+    }
+
+    @Test
+    public void giveAdministratorStatus() {
+    }
+
+    @Test
+    public void takeAwayAdministratorStatus() {
+    }
+
+    @Test
+    public void deleteGroup() {
+    }
+}
